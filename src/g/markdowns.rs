49d198e0d15df5adbcf4d83b59ede9dd60cd5d33
vhaoran <183455715@qq.com>
2025-12-18 18:18:19 +0800

    aaaa

--- src/g/markdowns.rs
+++ src/g/markdowns.rs
@@ -24,22 +24,27 @@ where
     s
 }
 
+pub fn md_escape<T:AsRef<str>>(s: T) -> String {
+    self::markdown_v_escape(s.as_ref())
+}
+
 pub fn markdown_v_escape(s: &str) -> String {
-    let markup = vec![
-        "\\", //	backslash
-        "`",  //	backtick (see also escaping backticks in code)
-        "*",  //	asterisk
-        "_",  //	underscore
-        "{", "}", //	curly braces
-        "[", "]", //	brackets
-        "(", ")", //	parentheses
-        "#", //	pound sign
-        "+", //	plus sign
-        "-", //	minus sign (hyphen)
-        ".", //	dot
-        "!", //	exclamation mark
-        "|", //	pipe (se
-    ];
+    // let markup = vec![
+    //     "\\", //	backslash
+    //     "`",  //	backtick (see also escaping backticks in code)
+    //     "*",  //	asterisk
+    //     "_",  //	underscore
+    //     "{", "}", //	curly braces
+    //     "[", "]", //	brackets
+    //     "(", ")", //	parentheses
+    //     "#", //	pound sign
+    //     "+", //	plus sign
+    //     "-", //	minus sign (hyphen)
+    //     ".", //	dot
+    //     "!", //	exclamation mark
+    //     "|", //	pipe (se
+    // ];
+    let markup = vec!["*", "\\", "_", "`", "["];
     let mut s = s.to_string();
     for c in markup {
         if s.contains(c) {
